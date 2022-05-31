b7e4f62d30510cf07fa1a449875fc94df2b7c4b7
Frisianrage <65295703+Frisianrage@users.noreply.github.com>
2022-05-31 10:24:09 -0700

    DS_I No.53

---
+++ DataStructure_I/53_Maximum_Subarray/solution.js
@@ -0,0 +1,44 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+ var maxSubArray = function(nums) {
+    function maxSum(min, max) {
+       if (min < max) {
+           // the current middle index
+           const mid = Math.floor((min+max)/2)
+           
+           // the three sums to compare
+           const leftSum = maxSum(min, mid)
+           const rightSum = maxSum(mid+1, max)
+           const crossSum = maxCrossSum(min, mid, max)
+           
+           // choosing the biggest one
+           const leftOrRight = Math.max(leftSum, rightSum)
+           return Math.max(leftOrRight, crossSum)
+       }
+       // if min == max, return the first or single value 
+       else return nums[min]
+   }
+   
+   function maxCrossSum(min, mid, max) {
+       // find left side max
+       let maxLeft = sumLeft = nums[mid]        
+       for(let i = mid-1; i >= min; i--) {  
+           sumLeft = sumLeft + nums[i]
+           maxLeft = (sumLeft > maxLeft) ? sumLeft : maxLeft
+       }
+       
+       // find right side max
+       let maxRight = sumRight = nums[mid+1]
+       for(let i = mid+2; i <= max; i++) {
+           sumRight = sumRight + nums[i]
+           maxRight = (sumRight > maxRight) ? sumRight : maxRight
+       }
+
+       return maxLeft + maxRight
+   }
+       
+   // call the recursive function for the full nums array
+   return maxSum(0, nums.length-1)
+};
